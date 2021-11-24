a9ed27e2d2f323d869f80f70f3830e9be7f2cb39
Regev Agabi <regev.agabi@gmail.com>
2021-11-24 10:46:24 -0800

    half way

--- app/main.go
+++ app/main.go
@@ -1,15 +1,13 @@
 package main
 
-import "log"
-
 // type appContext struct {
 // 	sess  *session.Session
 // 	dbSvc *dynamodb.DynamoDB
 // }
-func (a *App) start() {
-	log.Println("Server is Running on port 5000")
-	a.r.HandleFunc()
-}
+// func (a *App) start() {
+// 	// log.Println("Server is Running on port 5000")
+// 	// a.r.HandleFunc("/health", "", "")
+// }
 func main() {
 	listTable()
 	getSecret()
